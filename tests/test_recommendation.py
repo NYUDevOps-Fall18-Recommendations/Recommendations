df8a0a8428d23da9aa6e1784d3fe58847318b18d
yc2945 <yc2945@nyu.edu>
2018-10-11 18:21:50 -0400

    added delete method

---
+++ tests/test_recommendation.py
@@ -0,0 +1,27 @@
+"""
+Recommendation Test Suite
+Test cases can be run with the following:
+nosetests -v --with-spec --spec-color
+"""
+
+import os
+import json
+import unittest
+from models import Recommendation, DataValidationError
+
+######################################################################
+#  T E S T   C A S E S
+######################################################################
+class TestRecommendations(unittest.TestCase):
+	""" Test Cases for Recommendation Model """
+
+	def setUp(self):
+		# Recommendation.init_db()
+		Recommendation.remove_all()
+
+	def test_delete_a_recommendation(self): 
+		recommendation = Recommendation(0, "name", "recommended", "category")
+		recommended.save()
+		self.assertEqual(len(Recommendation.all()), 1)
+		recommendation.delete()
+		self.assertEqual(len(Recommendation.all()), 1)

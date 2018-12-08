e01020988b9fb35598c82b84e31c68359596c2e0
Amery Chang <changamery@gmail.com>
2018-12-08 17:43:39 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ Start the Pet Service and initializes logging
 """
 
 import os
-from app import app, server
+from service import app, service
 
 # Pull options from environment
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     print "*************************************************************"
     print " R E C O M M E N D A T I O N   S E R V I C E   R U N N I N G"
     print "*************************************************************"
-    server.initialize_logging()
-    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
+    #service.initialize_logging()
+    service.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)

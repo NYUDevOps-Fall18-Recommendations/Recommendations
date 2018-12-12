7e233eaf17657df04503083006712990c29ee520
Raj Sekhar Bandhakavi <37255023+rajBandhakavi@users.noreply.github.com>
2018-12-12 11:39:43 -0500

    Update recommendation_steps.py
    Added context to visit homepage. (Minor bug)

--- features/steps/recommendation_steps.py
+++ features/steps/recommendation_steps.py
@@ -36,6 +36,7 @@ def step_impl(context):
 def step_impl(context):
     """ Make a call to the base URL """
     context.driver.get(context.base_url)
+    context.resp = requests.get(context.base_url)
     #context.driver.save_screenshot('home_page.png')
 
 @then('I should see "{message}" in the title')
